85995c4bb1ddeed936de728d2256199466bddd26
upalinski <ulad@cere.io>
2021-05-26 18:22:22 +0300

    DdcConsumer checkpoints + autocommit

--- src/main/kotlin/network/cere/ddc/client/consumer/Consumer.kt
+++ src/main/kotlin/network/cere/ddc/client/consumer/Consumer.kt
@@ -3,5 +3,19 @@ package network.cere.ddc.client.consumer
 import io.smallrye.mutiny.Multi
 
 interface Consumer {
-    fun consume(streamId: String, dataQuery: DataQuery): Multi<Piece>
+    fun consume(streamId: String, dataQuery: DataQuery): Multi<ConsumerRecord>
+
+    /*
+    If enableAutoCommit is set to false - resolve checkpoint should be triggered by client when piece was successfully
+    processed. Otherwise, checkpoint won't be saved to storage and after failure or restart consumer will start consuming
+    from the beginning instead of checkpoint.
+
+    If enableAutoCommit is set to true - consumer commits checkpoints each autoCommitIntervalMs.
+     */
+    fun commitCheckpoint(streamId: String, consumerRecord: ConsumerRecord)
+
+    /*
+    Closes consumer streams
+     */
+    fun close()
 }
---
+++ src/main/kotlin/network/cere/ddc/client/consumer/ConsumerConfig.kt
@@ -0,0 +1,12 @@
+package network.cere.ddc.client.consumer
+
+data class ConsumerConfig(
+    val appPubKey: String,
+    val bootstrapNodes: List<String>,
+    val partitionPollIntervalMs: Int = 5_000,
+    // number of threads in partition poll executor (number of partitions to be consumed in parallel)
+    val partitionPollExecutorSize: Int = 4,
+    val updateAppTopologyIntervalMs: Int = 30_000,
+    val enableAutoCommit: Boolean = true,
+    val autoCommitIntervalMs: Int = 5_000,
+)
