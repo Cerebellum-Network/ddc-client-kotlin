02e844b18830038cb39dba5401c0fcff8366c317
upalinski <ulad@cere.io>
2021-05-31 11:20:04 +0300

    native-image compatibility for SendPieceResponse

--- src/main/kotlin/network/cere/ddc/client/producer/SendPieceResponse.kt
+++ src/main/kotlin/network/cere/ddc/client/producer/SendPieceResponse.kt
@@ -1,3 +1,8 @@
 package network.cere.ddc.client.producer
 
-data class SendPieceResponse(val cid: String)
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class SendPieceResponse(
+    @field:JsonProperty("cid")
+    val cid: String? = null
+)
