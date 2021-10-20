65bf81b6dd8cab129f01305cb9d801ce68f01fbe
svalasovich <svalasovich@gmail.com>
2021-10-20 17:52:11 +0300

    updated getAppTopology method

--- src/main/kotlin/network/cere/ddc/client/common/MetadataManager.kt
+++ src/main/kotlin/network/cere/ddc/client/common/MetadataManager.kt
@@ -1,57 +1,64 @@
 package network.cere.ddc.client.common
 
 import io.netty.handler.codec.http.HttpResponseStatus
-import io.smallrye.mutiny.Multi
-import io.smallrye.mutiny.tuples.Tuple4
-import io.smallrye.mutiny.unchecked.Unchecked
+import io.smallrye.mutiny.Uni
 import io.vertx.mutiny.ext.web.client.WebClient
 import io.vertx.mutiny.ext.web.codec.BodyCodec
 import network.cere.ddc.client.api.AppTopology
 import network.cere.ddc.client.api.PartitionTopology
+import network.cere.ddc.client.common.exception.AppTopologyException
 import org.slf4j.LoggerFactory
-import java.io.IOException
-import java.net.ConnectException
+import java.lang.Long.max
 import java.util.concurrent.ConcurrentLinkedDeque
 import java.util.concurrent.ConcurrentSkipListSet
+import java.util.function.Consumer
 import java.util.zip.CRC32
 
 class MetadataManager(
     bootstrapNodes: List<String>,
     private val client: WebClient,
     private val retries: Int,
-    connectionNodesCacheSize: Int,
+    private val connectionNodesCacheSize: Int,
 ) {
+
+    private val addressesDeque = ConcurrentLinkedDeque<String>()
+    private val addressesSet = ConcurrentSkipListSet<String>()
+
+    init {
+        val nodes = bootstrapNodes.map { it.removeSuffix("/") }
+        addressesDeque.addAll(nodes)
+        addressesSet.addAll(nodes)
+    }
+
 
     private val log = LoggerFactory.getLogger(javaClass)
 
-    private val nodes = ConcurrentDynamicNodeList(connectionNodesCacheSize, bootstrapNodes.map { it.removeSuffix("/") })
-
     fun getAppTopology(appPubKey: String): AppTopology {
-        val appTopology = nodes.getValues()
-            .onItem().transformToUni {
-                client.getAbs("$it/api/rest/apps/${appPubKey}/topology")
-                    .`as`(BodyCodec.json(AppTopology::class.java))
-                    .send()
-                    .onFailure(ConnectException::class.java).recoverWithNull()
-            }.merge(1).map(Unchecked.function { res ->
-                if (res.statusCode() != HttpResponseStatus.OK.code()) {
-                    log.warn("Can't load app topology (statusCode=${res.statusCode()}, body=${res.bodyAsString()})")
-                    throw IOException("Can't load app topology")
-                }
-
-                res.body()
-            }).onFailure().retry().atMost(retries.toLong())
-            .onFailure(NoSuchElementException::class.java).transform {
-                log.error("Can't load app topology. All known nodes are unavailable. message=${it.message}")
-                IOException("Can't load app topology. All known nodes are unavailable.")
+        val appTopology = Uni.createFrom().deferred {
+            val address = addressesDeque.peek()
+            client.getAbs("$address/api/rest/apps/${appPubKey}/topology")
+                .`as`(BodyCodec.json(AppTopology::class.java))
+                .send()
+                .onFailure().transform { AppTopologyException("Can't connect to node", address, it) }
+                .map { address to it }
+        }.onItem().transform { pair ->
+            if (pair.second.statusCode() != HttpResponseStatus.OK.code()) {
+                throw AppTopologyException("Bad response from node (statusCode=${pair.second.statusCode()}, body=${pair.second.bodyAsString()})", pair.first)
             }
-            .collect().first().await().indefinitely()
 
-
-        appTopology?.partitions?.forEach { partition ->
-            partition.replicas?.forEach { nodes.add(it.nodeHttpAddress) }
-            nodes.add(partition.master?.nodeHttpAddress)
+            pair.second.body()
         }
+            .onFailure(AppTopologyException::class.java).invoke { ex ->
+                val exception = ex as AppTopologyException
+                log.warn("Can't load app topology from node address=${exception.address}")
+                exception.address?.also { moveAddressToLast(it) }
+            }
+            .onFailure().retry().atMost(max(retries.toLong(), 1) * addressesSet.size - 1)
+            .onFailure().transform { ex -> AppTopologyException("App topology is not available from nodes", ex) }
+            .onFailure().invoke(Consumer { log.error("Couldn't load App from any node", it) })
+            .runSubscriptionOn { Thread(it).start() }.await().indefinitely()
+
+        updateNodeAddresses(appTopology)
 
         return appTopology
     }
@@ -67,39 +74,22 @@ class MetadataManager(
         return appTopology.partitions!!.filter { it.sectorStart!! <= ringToken && ringToken <= it.sectorEnd!! }
     }
 
-    private class ConcurrentDynamicNodeList(
-        private val size: Int,
-        startValues: Collection<String> = emptyList()
-    ) {
-        private val addressDeque = ConcurrentLinkedDeque(startValues)
-        private val addressSet = ConcurrentSkipListSet(startValues)
-
-        fun getValues(): Multi<String> {
-            return Multi.createFrom().generator<Tuple4<MutableIterator<String>, Int, String, Int>?, String?>(
-                { Tuple4.of(addressDeque.iterator(), addressDeque.size, null as String?, 0) }
-            ) { state, emitter ->
-                state.item3?.also { moveToLast(it) }
-                val item = state.item1
-                    .takeIf { state.item4 < state.item2 && state.item1.hasNext() }
-                    ?.next()
-
-                item?.also { emitter.emit(it) } ?: emitter.fail(NoSuchElementException("No more nodes to connect"))
-
-                Tuple4.of(state.item1, state.item2, item, state.item4 + 1)
+    private fun updateNodeAddresses(appTopology: AppTopology) {
+        if (addressesSet.size < connectionNodesCacheSize) {
+            appTopology.partitions?.forEach { partition ->
+                addAddress(partition.master?.nodeHttpAddress)
+                partition.replicas?.forEach { addAddress(it.nodeHttpAddress) }
             }
         }
+    }
 
-        fun add(nodeHttpAddress: String?) = nodeHttpAddress
-            ?.let { addressSet.size < size && addressSet.add(nodeHttpAddress) && addressDeque.add(nodeHttpAddress) }
+    private fun addAddress(address: String?) =
+        address != null && addressesSet.size < connectionNodesCacheSize
+                && addressesSet.add(address) && addressesDeque.add(address)
 
-        private fun moveToLast(nodeHttpAddress: String) {
-            if (addressDeque.size <= 1 || addressDeque.peek() != nodeHttpAddress) {
-                return
-            }
-
-            if (addressDeque.removeFirstOccurrence(nodeHttpAddress)) {
-                addressDeque.add(nodeHttpAddress)
-            }
+    private fun moveAddressToLast(address: String) {
+        if (addressesDeque.size > 1 && addressesDeque.removeFirstOccurrence(address)) {
+            addressesDeque.add(address)
         }
     }
 }
