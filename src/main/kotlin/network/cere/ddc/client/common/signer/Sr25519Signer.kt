e52834cd83a12ea82c22de13bf186d9287b8c65f
upalinski <ulad@cere.io>
2021-11-24 22:39:07 +0300

    create signing context locally

--- src/main/kotlin/network/cere/ddc/client/common/signer/Sr25519Signer.kt
+++ src/main/kotlin/network/cere/ddc/client/common/signer/Sr25519Signer.kt
@@ -6,11 +6,10 @@ import com.debuggor.schnorrkel.sign.SigningContext
 
 class Sr25519Signer(privateKey: String): Signer {
 
-    private val signingContext = SigningContext.createSigningContext("substrate".toByteArray())
-
     private val keyPair = KeyPair.fromSecretSeed(privateKey.hexToBytes(), ExpansionMode.Ed25519)
 
     override fun sign(data: String): String {
+        val signingContext = SigningContext.createSigningContext("substrate".toByteArray())
         return keyPair.sign(signingContext.bytes(data.toByteArray())).to_bytes().toHex()
     }
 
