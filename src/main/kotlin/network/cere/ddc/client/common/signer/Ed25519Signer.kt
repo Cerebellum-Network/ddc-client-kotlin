bb00ce4fd15a58645d92b5fc63ca1145e9edaff7
upalinski <ulad@cere.io>
2021-11-25 07:50:04 +0300

    clean up ed25519 signer

--- src/main/kotlin/network/cere/ddc/client/common/signer/Ed25519Signer.kt
+++ src/main/kotlin/network/cere/ddc/client/common/signer/Ed25519Signer.kt
@@ -1,12 +1,10 @@
 package network.cere.ddc.client.common.signer
 
 import org.bouncycastle.math.ec.rfc8032.Ed25519
-import org.bouncycastle.util.encoders.Hex
 
 class Ed25519Signer(privateKey: String) : Signer {
 
-    private val private: ByteArray =
-        Hex.decode(privateKey.removePrefix("0x")).sliceArray(0 until Ed25519.SECRET_KEY_SIZE)
+    private val private: ByteArray = privateKey.hexToBytes().sliceArray(0 until Ed25519.SECRET_KEY_SIZE)
     private val public: ByteArray = ByteArray(Ed25519.PUBLIC_KEY_SIZE).also {
         Ed25519.generatePublicKey(private, 0, it, 0)
     }
@@ -17,7 +15,7 @@ class Ed25519Signer(privateKey: String) : Signer {
 
         Ed25519.sign(private, 0, public, 0, bytes, 0, bytes.size, signatureBytes, 0)
 
-        return Hex.toHexString(signatureBytes)
+        return signatureBytes.toHex()
     }
 
 }
